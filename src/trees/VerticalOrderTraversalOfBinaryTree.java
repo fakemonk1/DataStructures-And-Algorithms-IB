5117940d4b426b69ef318cdfa866d259b6b07cfb
Ashish Gupta <ashish.gupta@rokittech.com>
2017-04-10 00:27:52 +0530

    added Tree Solutions

---
+++ src/trees/VerticalOrderTraversalOfBinaryTree.java
@@ -0,0 +1,51 @@
+package trees;
+
+import java.util.*;
+
+public class VerticalOrderTraversalOfBinaryTree {
+    public ArrayList<ArrayList<Integer>> verticalOrderTraversal(TreeNode root) {
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Map<Integer, ArrayList<Integer>> map = new HashMap<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        Queue<Integer> cols = new LinkedList<>();
+
+        q.add(root);
+        cols.add(0);
+
+        int min = 0;
+        int max = 0;
+
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            int col = cols.poll();
+
+            if (!map.containsKey(col)) {
+                map.put(col, new ArrayList<Integer>());
+            }
+
+            map.get(col).add(node.val);
+
+            if (node.left != null) {
+                q.add(node.left);
+                cols.add(col - 1);
+                min = Math.min(min, col - 1);
+            }
+
+            if (node.right != null) {
+                q.add(node.right);
+                cols.add(col + 1);
+                max = Math.max(max, col + 1);
+            }
+        }
+        for (int i = min; i <= max; i++) {
+            res.add(map.get(i));
+        }
+
+        return res;
+    }
+}
---
+++ src/trees/ZigZagLevelOrderTraversalBT.java
@@ -0,0 +1,79 @@
+package trees;
+
+import java.util.ArrayList;
+
+public class ZigZagLevelOrderTraversalBT {
+
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        ArrayList<ArrayList<Integer>> solution = new ArrayList<>();
+        if (root == null)
+            return solution;
+        int order = 0;
+        ArrayList<TreeNode> current = new ArrayList<>();
+        current.add(root);
+        while (!current.isEmpty()) {
+            ArrayList<TreeNode> children = new ArrayList<>();
+            for (TreeNode node : current) {
+                if (node.left != null)
+                    children.add(node.left);
+                if (node.right != null)
+                    children.add(node.right);
+            }
+            order = 1- order;
+            solution.add(getList(current, order));
+            current = children;
+        }
+
+        return solution;
+    }
+
+
+    public ArrayList<Integer> getList(ArrayList<TreeNode> input, int order) {
+
+        if (order == 0) {
+            return getReverseOrder(input);
+        }
+        return getNormalOrder(input);
+    }
+
+    public ArrayList<Integer> getNormalOrder(ArrayList<TreeNode> input) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < input.size(); i++) {
+            answer.add(input.get(i).val);
+        }
+        return answer;
+    }
+
+    public ArrayList<Integer> getReverseOrder(ArrayList<TreeNode> input) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (int i = input.size() - 1; i >= 0; i--) {
+            answer.add(input.get(i).val);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        TreeNode a1 = new TreeNode(1);
+        TreeNode a2 = new TreeNode(2);
+        TreeNode a3 = new TreeNode(3);
+        TreeNode a4 = new TreeNode(4);
+        TreeNode a5 = new TreeNode(5);
+        TreeNode a6 = new TreeNode(6);
+        TreeNode a7 = new TreeNode(7);
+        TreeNode a8 = new TreeNode(8);
+
+        a1.left = a2;
+        a1.right = a3;
+
+        a2.left = a4;
+        a2.right = a5;
+
+        a3.right = a6;
+
+        a4.left = a7;
+        a4.right = a8;
+
+        System.out.println(new ZigZagLevelOrderTraversalBT().zigzagLevelOrder(a1));
+    }
+}
